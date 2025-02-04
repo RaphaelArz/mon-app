31f3f30741096ad6fdc4f66440105c10605f0b4f
Raphael <raphaelarzoin173@gmail.com>
2025-02-04 12:17:08 +0100

    tt

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+/* test('renders learnf react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/raphael le bg/i);
+  expect(linkElement).toBeInTheDocument();
+});
+ */
